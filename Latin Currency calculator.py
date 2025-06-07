417ab059466d5273b899617061bdbed00b19d74e
Chudi1475 <chudid777@gmail.com>
2025-06-07 18:52:25 -0500

    Initial commit: Add Latin Currency calculator script

---
+++ Latin Currency calculator.py
@@ -0,0 +1,5 @@
+#Creatiing currency converter for colobian pesos to us dollars
+#peruvian soles to us dollars
+#brazilian reais to us dollars
+
+#First we need to get the exchange rates from the internet 
