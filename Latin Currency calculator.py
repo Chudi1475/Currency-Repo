793b95731987b5ce380ab48e6ad5c63e6b49ffc4
Chudi1475 <chudid777@gmail.com>
2025-06-07 19:15:41 -0500

    Leftover Currency

--- Latin Currency calculator.py
+++ Latin Currency calculator.py
@@ -8,5 +8,12 @@ rate_cop_to_usd = 1 / 4115
 rate_pen_to_usd = 1 / 3.61
 rate_brl_to_usd = 1 / 5.58
 
+#Now we need to get the leftover currency from the user
+
+cop_leftover = float(input('What do you have left in Colombian pesos? '))
+pen_leftover = float(input('What do you have left in Peruvian soles? '))
+brl_leftover = float(input('What do you have left in Brazilian reais? '))
+
+
 
 
